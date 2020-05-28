7cb995c80dfe0822ec5fbe4e565610ec3d9f3017
aldiandika <aldi.andika@gmail.com>
2020-05-28 16:31:36 +0700

    Tambah tampilan dasar

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import kivy
 kivy.require('1.11.1')
 
-from kivy.app import App
-from kivy.uix.label import Label
+from kivymd.app import MDApp
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
 
-class MyApp(App):
+from kivymd.uix.label import MDLabel
+from kivymd.uix.button import MDRectangleFlatButton
+
+Window.size = (1020, 576)
+# Window.fullscreen = True
+
+class HomePage(BoxLayout):
+    pass
+
+class HomeApp(MDApp):
     def build(self):
-        return Label(text='Hello World')
+        return HomePage()
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    HomeApp().run()
